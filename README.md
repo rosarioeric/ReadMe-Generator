535414eb8cac7348fa53e95044a629c6652b3e21
Eric Rosario <steven.e.rosario@gmail.com>
2020-10-05 08:50:42 -0700

    readme genertor

--- README.md
+++ README.md
@@ -1,33 +1,13 @@
-<!doctype html>
-        <html lang="en">
-        <head>
-            <!-- Required meta tags -->
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-            <!-- Bootstrap CSS -->
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
-            <title>Hello, world!</title>
-        </head>
-
-        <body>
-        <div class="jumbotron">
-        <h1 class="display-4">Title: dsfa</h1>
-        <h1 class="display-4">Description: asdfa</h1>
-        <h1 class="display-4">Table of Contents: asdfa</h1>
-        <h1 class="display-4">Installation: asdfa</h1>
-        <h1 class="display-4">Usage: undefined</h1>
-        <h1 class="display-4">License: adsfa</h1>
-        <h1 class="display-4">Contributing: asdfa</h1>
-        <h1 class="display-4">Tests: asdfa</h1>
-        <h1 class="display-4">Questions: asdfas</h1>
-                <p>Github: adfa</p>
-                <p>Email me with any questions: asdf</p>
-            <!-- Optional JavaScript -->
-            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-        </body>
-        </html>
+#Title: ReadMe Generator
+       License: na
+       Description: Create a readme generator
+       
+       #Table of Contents: na
+        -Installation: na
+        -Usage: undefined
+        -Contributing: myself
+        -Tests: na
+       
+        #Questions: na
+        -Github: https://github.com/rosarioeric/ReadMe-Generator
+        -Email: steven.e.rosario@gmail.com
--- index.js
+++ index.js
@@ -63,39 +63,20 @@ inquirer.prompt([
         }
     })
 })
-
+//style this area with node commands
 function createHtml(userInput){
-       return `<!doctype html>
-        <html lang="en">
-        <head>
-            <!-- Required meta tags -->
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-            <!-- Bootstrap CSS -->
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
-            <title>Hello, world!</title>
-        </head>
-
-        <body>
-        <div class="jumbotron">
-        <h1 class="display-4">Title: ${userInput.title}</h1>
-        <h1 class="display-4">Description: ${userInput.description}</h1>
-        <h1 class="display-4">Table of Contents: ${userInput.tableOfContents}</h1>
-        <h1 class="display-4">Installation: ${userInput.installation}</h1>
-        <h1 class="display-4">Usage: ${userInput.usage}</h1>
-        <h1 class="display-4">License: ${userInput.license}</h1>
-        <h1 class="display-4">Contributing: ${userInput.contributing}</h1>
-        <h1 class="display-4">Tests: ${userInput.tests}</h1>
-        <h1 class="display-4">Questions: ${userInput.questions}</h1>
-                <p>Github: ${userInput.github}</p>
-                <p>Email me with any questions: ${userInput.email}</p>
-            <!-- Optional JavaScript -->
-            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-        </body>
-        </html>`
-        }
+        
+return`#Title: ${userInput.title}
+       License: ${userInput.license}
+       Description: ${userInput.description}
+       
+       #Table of Contents: ${userInput.tableOfContents}
+        -Installation: ${userInput.installation}
+        -Usage: ${userInput.usage}
+        -Contributing: ${userInput.contributing}
+        -Tests: ${userInput.tests}
+       
+        #Questions: ${userInput.questions}
+        -Github: ${userInput.github}
+        -Email: ${userInput.email}`
+}
